6b0b0e10d0688a9060274078092e3cf32d0a1648
Amry Amanah <amry.amanah@gmail.com>
2022-04-05 11:14:26 +0700

    Add GatewayUrl and TableViewerUrl output

--- lib/cdkts_workshop-stack.ts
+++ lib/cdkts_workshop-stack.ts
@@ -6,6 +6,9 @@ import { TableViewer } from 'cdk-dynamo-table-viewer';
 import { Construct } from 'constructs';
 
 export class CdktsWorkshopStack extends cdk.Stack {
+  public readonly hcViewerUrl: cdk.CfnOutput;
+  public readonly hcEndpoint: cdk.CfnOutput;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -21,13 +24,21 @@ export class CdktsWorkshopStack extends cdk.Stack {
     });
 
     // defines an API Gatewat REST API resource backed by our "hello" function.
-    new apigw.LambdaRestApi(this, 'Endpoint', {
+    const gateway = new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler
     });
 
-    new TableViewer(this, 'ViewHitCounter', {
+    const tv = new TableViewer(this, 'ViewHitCounter', {
       title: 'Hello Hits',
       table: helloWithCounter.table
     })
+
+    this.hcEndpoint = new cdk.CfnOutput(this, "GatewayUrl", {
+      value: gateway.url
+    });
+
+    this.hcViewerUrl = new cdk.CfnOutput(this, "TableViewerUrl", {
+      value: tv.endpoint
+    });
   }
 }
